7b987ffe213faff17875ccabbf2f6967260b0691
Vishal Chaudhary <vishals08122003@gmail.com>
2025-05-14 14:24:29 +0530

    Created different Modules,Fixed Price formatting and radio selectors

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,22 @@
-export const cart = [];
+import { products } from "../data/products.js";
+
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+},{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 5
+}];
 
 export function addToCart(productId){
-  
-      let matchingItem = '';
+    let matchingItem = '';
 
-      cart.forEach((cartItem) => {
-        if(productId === cartItem.productId){
-          matchingItem = cartItem;
-        }
-      });
-      
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productId){
+        matchingItem = cartItem;
+      }
+    });
+    
     //Quanitiy Selector
     let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
 
