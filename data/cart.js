42811f8e27b2047bcf7cd906c171f26c972d7d6c
Vishal Chaudhary <vishals08122003@gmail.com>
2025-06-20 23:14:41 +0530

    Save data and Generate HTML for DeliveryOptions

--- data/cart.js
+++ data/cart.js
@@ -5,10 +5,12 @@ export let cart = JSON.parse(localStorage.getItem('cart')); //retrives the cart 
 if(!cart){ //if cart is empty, we will display some default products in cart
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
   },{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 5
+    quantity: 5,
+    deliveryOptionId: '3'
   }];
 }
 
@@ -16,7 +18,7 @@ if(!cart){ //if cart is empty, we will display some default products in cart
     localStorage.setItem('cart',JSON.stringify(cart));
   }
 
-//Ading Products in Cart
+//Adding Products in Cart
 export function addToCart(productId){
     let matchingItem = '';
 
@@ -37,14 +39,15 @@ export function addToCart(productId){
     } else {
       cart.push({
         productId,
-        quantity
+        quantity,
+        deliveryOptionId: '1'
       });
     }
 
     saveToStorage();
   }
   
-//Removing Priouct form a cart
+//Removing Product form a cart
 export function removeFromCart(productId){
   const newCart = []; // Creates a new empty array
 
