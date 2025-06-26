bb99dba921e820f1ad849f6a748297dfc0faf9df
Vishal Chaudhary <vishals08122003@gmail.com>
2025-06-27 01:48:33 +0530

    Create OOP version of Cart

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -1,107 +1,231 @@
-const cart = {
-  cartItems: JSON.parse(localStorage.getItem('cart')), //retrives the cart products if saved earlier
-
-};
- 
-if(!cart){ //if cart is empty, we will display some default products in cart
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  },{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '3'
-  }];
-}
-
-  function saveToStorage(){
-    localStorage.setItem('cart',JSON.stringify(cart));
-  }
-
-//Adding Products in Cart
-export function addToCart(productId){
-    let matchingItem = '';
-
-    cart.forEach((cartItem) => {
-      if(productId === cartItem.productId){
-        matchingItem = cartItem;
+// import { products } from "./products.js";
+function Cart(localStorageKey){
+  const cart = {
+    cartItems: undefined, //retrives the cart products if saved earlier
+    
+    loadFromStorage(){       
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+      if(!this.cartItems){ //if cart is empty, we will display some default products in cart
+        this.cartItems = [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        },{
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '3'
+        }];
       }
-    });
-    
-    //Quanitiy Selector
-    let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-
-    let quantity = Number(quantitySelector.value) || 1;
-    
-    if(matchingItem){
-      matchingItem.quantity += quantity;
-      console.log(quantity);
-    } else {
-      cart.push({
-        productId,
-        quantity,
-        deliveryOptionId: '1'
-      });
-    }
-
-    saveToStorage();
-  }
+    },
   
-//Removing Product form a cart
-export function removeFromCart(productId){
-  const newCart = []; // Creates a new empty array
-
-  /* Instead of directly deleting an item from the existing cart array,we are creating a new array (newCart).
-     Then, it loops through the original cart:
-     If a product’s id matches the one being deleted, it is ignored.
-     All other products are added to newCart. */
-  cart.forEach((cartItem) => { 
-    if(cartItem.productId !== productId){ // Keeps only items that DON'T match the given productId
-      newCart.push(cartItem); //// Adds those items to the new cart
+    saveToStorage(){
+      localStorage.setItem(localStorageKey,JSON.stringify(this.cartItems));
+    },
+  
+    //Adding Products in Cart
+    addToCart(productId){
+        let matchingItem = '';
+    
+        this.cartItems.forEach((cartItem) => {
+          if(productId === cartItem.productId){
+            matchingItem = cartItem;
+          }
+        });
+        
+        //Quanitiy Selector
+        let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    
+        let quantity = Number(quantitySelector.value) || 1;
+        
+        if(matchingItem){ //if product is already present, update quantity
+          matchingItem.quantity += quantity;
+        } else { //if not present in cart Push it in cart
+          this.cartItems.push({
+            productId,
+            quantity,
+            deliveryOptionId: '1'
+          });
+        }
+    
+        this.saveToStorage();
+    },
+    
+    //Removing Product form a cart
+    removeFromCart(productId){
+      const newCart = []; // Creates a new empty array
+    
+      /* Instead of directly deleting an item from the existing cart array,we are creating a new array (newCart).
+         Then, it loops through the original cart:
+         If a product’s id matches the one being deleted, it is ignored.
+         All other products are added to newCart. */
+      this.cartItems.forEach((cartItem) => { 
+        if(cartItem.productId !== productId){ // Keeps only items that DON'T match the given productId
+          newCart.push(cartItem); //// Adds those items to the new cart
+        }
+      });
+      this.cartItems = newCart; // Replaces the old cart with the updated one
+    
+      this.saveToStorage();
+    },
+  
+    //Calculate Total Cart Quantity
+    calculateCartQuantity() {
+      let cartQuantity = 0;
+    
+      this.cartItems.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+    
+      return cartQuantity;
+    },
+    
+    //Finds a matching productId in cart and updates its quantity to new Quantity
+    updateQuantity(productId,newQuantity){
+      let matchingItem;
+    
+      this.cartItems.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.quantity = newQuantity;
+    
+      this.saveToStorage();
+    },
+    
+    updateDeliveryOption(productId,deliveryOptionId){
+      let matchingItem = '';
+    
+      this.cartItems.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.deliveryOptionId = deliveryOptionId;
+    
+      this.saveToStorage();
     }
-  });
-  cart = newCart; // Replaces the old cart with the updated one
+  };
 
-  saveToStorage();
-}
+  return cart;
 
-//Calculate Total Cart Quantity
-export function calculateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  return cartQuantity;
-}
-
-//Finds a matching productId in cart and updates its quantity to new Quantity
-export function updateQuantity(productId,newQuantity){
-  let matchingItem;
-
-  cart.forEach((cartItem) => {
-    if(productId === cartItem.productId){
-      matchingItem = cartItem;
+  const businessCart = {
+    cartItems: undefined, //retrives the cart products if saved earlier
+    
+    loadFromStorage(){       
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+      if(!this.cartItems){ //if cart is empty, we will display some default products in cart
+        this.cartItems = [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        },{
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '3'
+        }];
+      }
+    },
+  
+    saveToStorage(){
+      localStorage.setItem(localStorageKey,JSON.stringify(this.cartItems));
+    },
+  
+    //Adding Products in Cart
+    addToCart(productId){
+        let matchingItem = '';
+    
+        this.cartItems.forEach((cartItem) => {
+          if(productId === cartItem.productId){
+            matchingItem = cartItem;
+          }
+        });
+        
+        //Quanitiy Selector
+        let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    
+        let quantity = Number(quantitySelector.value) || 1;
+        
+        if(matchingItem){ //if product is already present, update quantity
+          matchingItem.quantity += quantity;
+        } else { //if not present in cart Push it in cart
+          this.cartItems.push({
+            productId,
+            quantity,
+            deliveryOptionId: '1'
+          });
+        }
+    
+        this.saveToStorage();
+    },
+    
+    //Removing Product form a cart
+    removeFromCart(productId){
+      const newCart = []; // Creates a new empty array
+    
+      /* Instead of directly deleting an item from the existing cart array,we are creating a new array (newCart).
+         Then, it loops through the original cart:
+         If a product’s id matches the one being deleted, it is ignored.
+         All other products are added to newCart. */
+      this.cartItems.forEach((cartItem) => { 
+        if(cartItem.productId !== productId){ // Keeps only items that DON'T match the given productId
+          newCart.push(cartItem); //// Adds those items to the new cart
+        }
+      });
+      this.cartItems = newCart; // Replaces the old cart with the updated one
+    
+      this.saveToStorage();
+    },
+  
+    //Calculate Total Cart Quantity
+    calculateCartQuantity() {
+      let cartQuantity = 0;
+    
+      this.cartItems.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+    
+      return cartQuantity;
+    },
+    
+    //Finds a matching productId in cart and updates its quantity to new Quantity
+    updateQuantity(productId,newQuantity){
+      let matchingItem;
+    
+      this.cartItems.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.quantity = newQuantity;
+    
+      this.saveToStorage();
+    },
+    
+    updateDeliveryOption(productId,deliveryOptionId){
+      let matchingItem = '';
+    
+      this.cartItems.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.deliveryOptionId = deliveryOptionId;
+    
+      this.saveToStorage();
     }
-  });
+  };
 
-  matchingItem.quantity = newQuantity;
-
-  saveToStorage();
+  return businessCart;
 }
 
-export function updateDeliveryOption(productId,deliveryOptionId){
-  let matchingItem = '';
-
-  cart.forEach((cartItem) => {
-    if(productId === cartItem.productId){
-      matchingItem = cartItem;
-    }
-  });
-
-  matchingItem.deliveryOptionId = deliveryOptionId;
-
-  saveToStorage();
-}
+const cart = Cart('cart-oop');
+const businessCart = Cart('businessCart');
+cart.loadFromStorage();
+businessCart.loadFromStorage();
+console.log(cart);
+console.log(businessCart);
